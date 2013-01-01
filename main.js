b84eb15a731e68fbbf52b79ea6f46c446e210695
Sven Nilsen <sven@cutoutpro.com>
2013-01-01 11:46:52 +0100

    Moved example.

--- main.js
+++ main.js
@@ -1,59 +1,26 @@
 
-var cirlcex = 0;
-var circley = 0;
-var lastPressed = 0;
-
 function load() {
-	document.title = "Example";
 	
-	app.graphics.setSize(600, 400);
+}
+
+function update() {
 	
-	app.timer.setFPS(60);
-	app.timer.setUPS(120);
+}
+
+function draw() {
+	
 }
 
 function keypressed(keyCode) {
-	var keyboard = app.keyboard;
 	
-	lastPressed = keyCode;
-	if (keyboard.isUp(keyCode)) lastPressed += " up";
-	if (keyboard.isRight(keyCode)) lastPressed += " right";
-	if (keyboard.isDown(keyCode)) lastPressed += " down";
-	if (keyboard.isLeft(keyCode)) lastPressed += " left";
-	if (keyboard.isSpace(keyCode)) lastPressed += " space";
-	if (keyboard.isAlt(keyCode)) lastPressed += " alt";
-	if (keyboard.isCtrl(keyCode)) lastPressed += " ctrl";
 }
 
 function keyreleased(keyCode) {
 	
 }
 
-function update() {
-	var time = app.timer.getTime();
-	circlex = Math.sin(time) * 100 + 100;
-}
-
-function draw() {
-	var g = app.graphics;
-	var timer = app.timer;
-	
-	g.clear("#000000");
-	
-	g.setColorRGBA(200, 10, 100, 255);
-	g.ellipse("fill", circlex, circley - 50, 100, 100);
-	g.setColor("white");
-	g.ellipse("fill", circlex+10, circley - 50 + 10, 80, 80);
-	
-	g.setColorRGBA(255, 255, 255, 255);
-	g.setFont("Arial", "normal", 50);
-	g.print("FPS: " + timer.getFPS(), 50, 200);
-	g.print("UPS: " + timer.getUPS(), 50, 250);
-	g.print("Last pressed: " + lastPressed, 50, 300);
-}
-
 function mousemove(x, y) {
-	circley = app.mouse.getY();
+	
 }
 
 function mousepressed(button, x, y) {
@@ -63,4 +30,5 @@ function mousepressed(button, x, y) {
 function mousereleased(button, x, y) {
 	
 }
+
 
