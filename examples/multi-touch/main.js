a4f7ac39a1515fd0abf855c9f119a49ca4778f5c
Sven Nilsen <sven@cutoutpro.com>
2013-01-07 09:58:00 +0100

    Added 'touchscreen.js'.

--- examples/multi-touch/main.js
+++ examples/multi-touch/main.js
@@ -1,77 +1,4 @@
 
-function load_touchscreen(box) {
-	var touchscreen = {};
-	
-	touchscreen.touches = [];
-	
-	document.ontouchstart = function() {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-	}
-	
-	document.ontouchend = function() {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-	}
-	
-	document.ontouchmove = function() {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-	}
-	
-	document.ontouchcancel = function() {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-	}
-	
-	touchscreen.numberOfFingers = function() {
-		return touchscreen.touches.length;
-	}
-	
-	touchscreen.getTouches = function() {
-		return touchscreen.touches;
-	}
-	
-	var updateTouches = function(event) {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-		
-		var targetTouches = event.targetTouches;
-		if (targetTouches == null || targetTouches.length == 0) {
-			if (touchscreen.touches.length != 0) touchscreen.touches = [];
-			return;
-		}
-		
-		touchscreen.touches = [];
-		for (var i = 0; i < targetTouches.length; i++) {
-			var x = targetTouches[i].clientX - box.offsetLeft;
-			var y = targetTouches[i].clientY - box.offsetTop;
-			touchscreen.touches[i] = {"x": x, "y": y};
-		}
-	}
-	
-	box.ontouchmove = function(event) {
-		updateTouches(event);
-		
-		if (typeof(touchmove) == "function") touchmove();
-	}
-	
-	box.ontouchstart = function(event) {
-		updateTouches(event);
-		
-		if (typeof(touchpressed) == "function") touchpressed();
-	}
-	
-	box.ontouchend = function(event) {
-		updateTouches(event);
-		
-		if (typeof(touchreleased) == "function") touchreleased();
-	}
-	
-	box.ontouchcancel = function(event) {
-		updateTouches(event);
-		
-		if (typeof(touchcancel) == "function") touchcancel();
-	}
-	
-	return touchscreen;
-}
-
 function load() {
 	
 }
@@ -128,19 +55,15 @@ function mousereleased(button, x, y) {
 }
 
 function touchmove() {
-	
 }
 
 function touchpressed() {
-	countFingers++;
 }
 
 function touchreleased() {
-	countFingers--;
 }
 
 function touchcancel() {
-	countFingers = 0;
 }
 
 
