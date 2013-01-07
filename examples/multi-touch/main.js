c138a501d99d52f6efdde7552b6b34fea0ebcf3f
Sven Nilsen <sven@cutoutpro.com>
2013-01-07 09:12:48 +0100

    Added 'touchcancel'.

--- examples/multi-touch/main.js
+++ examples/multi-touch/main.js
@@ -2,19 +2,60 @@
 function load_touchscreen(box) {
 	var touchscreen = {};
 	
+	touchscreen.touches = null;
+	touchscreen.targetTouches = null;
+	touchscreen.changedTouches = null;
+	
+	touchscreen.getTouches = function() {
+		return touchscreen.touches;
+	}
+	
+	touchscreen.getTargetTouches = function() {
+		return touchscreen.targetTouches;
+	}
+	
+	touchscreen.getChangedTouches = function() {
+		return touchscreen.changedTouches;
+	}
+	
 	box.ontouchmove = function(event) {
 		if (typeof(event.preventDefault) == "function") event.preventDefault();
+		
+		touchscreen.touches = event.touches;
+		touchscreen.targetTouches = event.targetTouches;
+		touchscreen.changedTouches = event.changedTouches;
+		
 		if (typeof(touchmove) == "function") touchmove();
 	}
 	
 	box.ontouchstart = function(event) {
 		if (typeof(event.preventDefault) == "function") event.preventDefault();
+		
+		touchscreen.touches = event.touches;
+		touchscreen.targetTouches = event.targetTouches;
+		touchscreen.changedTouches = event.changedTouches;
+		
 		if (typeof(touchpressed) == "function") touchpressed();
 	}
 	
 	box.ontouchend = function(event) {
 		if (typeof(event.preventDefault) == "function") event.preventDefault();
+		
+		touchscreen.touches = event.touches;
+		touchscreen.targetTouches = event.targetTouches;
+		touchscreen.changedTouches = event.changedTouches;
+		
 		if (typeof(touchreleased) == "function") touchreleased();
+	}
+	
+	box.ontouchcancel = function(event) {
+		if (typeof(event.preventDefault) == "function") event.preventDefault();
+		
+		touchscreen.touches = event.touches;
+		touchscreen.targetTouches = event.targetTouches;
+		touchscreen.changedTouches = event.changedTouches;
+		
+		if (typeof(touchcancel) == "function") touchcancel();
 	}
 	
 	return touchscreen;
@@ -30,12 +71,25 @@ function update() {
 	
 }
 
+function drawTouches() {
+	if (touches == null) return;
+	
+	var y = 40;
+	var touches = app.touchscreen.getTouches();
+	for (var i = 0; i < touches.length; i++) {
+		g.print("" + touches[i].pageX + ", " + touches[i].pageY, 0, y);
+		y += 20;
+	}
+}
+
 function draw() {
 	var g = app.graphics;
 	g.clear("#000000");
 	g.setColor("#FFFFFF");
 	g.setFont("georgia", "normal", 20);
 	g.print("Counting fingers: " + countFingers, 0, 20);
+	
+	drawTouches();
 }
 
 function keypressed(keyCode) {
@@ -68,6 +122,10 @@ function touchpressed() {
 
 function touchreleased() {
 	countFingers--;
+}
+
+function touchcancel() {
+	countFingers = 0;
 }
 
 
