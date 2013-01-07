35153e61c637eacfd5f17baa6b740a3002d15b06
Sven Nilsen <sven@cutoutpro.com>
2013-01-07 09:35:41 +0100

    Changed to custom coordinates.

--- examples/multi-touch/main.js
+++ examples/multi-touch/main.js
@@ -2,13 +2,9 @@
 function load_touchscreen(box) {
 	var touchscreen = {};
 	
-	touchscreen.touches = null;
-	touchscreen.targetTouches = null;
-	touchscreen.changedTouches = null;
+	touchscreen.touches = [];
 	
 	touchscreen.numberOfFingers = function() {
-		if (touchscreen.touches == null) return 0;
-		
 		return touchscreen.touches.length;
 	}
 	
@@ -16,50 +12,43 @@ function load_touchscreen(box) {
 		return touchscreen.touches;
 	}
 	
-	touchscreen.getTargetTouches = function() {
-		return touchscreen.targetTouches;
-	}
-	
-	touchscreen.getChangedTouches = function() {
-		return touchscreen.changedTouches;
+	var updateTouches = function(event) {
+		if (typeof(event.preventDefault) == "function") event.preventDefault();
+		
+		var targetTouches = event.targetTouches;
+		if (targetTouches == null || targetTouches.length == 0) {
+			if (touchscreen.touches.length != 0) touchscreen.touches = [];
+			return;
+		}
+		
+		touchscreen.touches = [];
+		for (var i = 0; i < targetTouches.length; i++) {
+			var x = targetTouches[i].clientX - box.offsetLeft;
+			var y = targetTouches[i].clientY - box.offsetTop;
+			touchscreen.touches[i] = {"x": x, "y": y};
+		}
 	}
 	
 	box.ontouchmove = function(event) {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-		
-		touchscreen.touches = event.touches;
-		touchscreen.targetTouches = event.targetTouches;
-		touchscreen.changedTouches = event.changedTouches;
+		updateTouches(event);
 		
 		if (typeof(touchmove) == "function") touchmove();
 	}
 	
 	box.ontouchstart = function(event) {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-		
-		touchscreen.touches = event.touches;
-		touchscreen.targetTouches = event.targetTouches;
-		touchscreen.changedTouches = event.changedTouches;
+		updateTouches(event);
 		
 		if (typeof(touchpressed) == "function") touchpressed();
 	}
 	
 	box.ontouchend = function(event) {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-		
-		touchscreen.touches = event.touches;
-		touchscreen.targetTouches = event.targetTouches;
-		touchscreen.changedTouches = event.changedTouches;
+		updateTouches(event);
 		
 		if (typeof(touchreleased) == "function") touchreleased();
 	}
 	
 	box.ontouchcancel = function(event) {
-		if (typeof(event.preventDefault) == "function") event.preventDefault();
-		
-		touchscreen.touches = event.touches;
-		touchscreen.targetTouches = event.targetTouches;
-		touchscreen.changedTouches = event.changedTouches;
+		updateTouches(event);
 		
 		if (typeof(touchcancel) == "function") touchcancel();
 	}
@@ -78,14 +67,14 @@ function update() {
 function drawTouches() {
 	var g = app.graphics;
 	var touches = app.touchscreen.getTouches();
-	if (touches == null) {
+	if (touches.length == 0) {
 		g.print("No touching", 0, 40);
 		return;
 	}
 	
 	var y = 40;
 	for (var i = 0; i < touches.length; i++) {
-		g.print("" + touches[i].clientX + ", " + touches[i].clientY, 0, y);
+		g.print("" + touches[i].x + ", " + touches[i].y, 0, y);
 		y += 20;
 	}
 }
